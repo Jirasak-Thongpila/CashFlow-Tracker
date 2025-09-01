28bdec8670884a17cf233f64a21fbb1921efd31b
Jirasak-Thongpire <tle0613828245@gmail.com>
2025-09-01 14:28:46 +0700

    make models

--- finance/models.py
+++ finance/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Income(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.description} - {self.amount} บาท"
+    
+    class Meta:
+        ordering = ['-date']
+
+class Expense(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.description} - {self.amount} บาท"
+    
+    class Meta:
+        ordering = ['-date']
