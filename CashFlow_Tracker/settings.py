8b9a23a70290c350365d6771939bab2719187cc4
Jirasak-Thongpire <tle0613828245@gmail.com>
2025-09-02 12:44:24 +0700

    Update database configuration to use PostgreSQL with environment variable for password

--- CashFlow_Tracker/settings.py
+++ CashFlow_Tracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,10 +76,21 @@ WSGI_APPLICATION = 'CashFlow_Tracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.ifbbqowbocrqeoqmflgo',
+        'PASSWORD': os.environ.get('SUPABASE_DB_PASSWORD', 'tT0613828245'),
+        'HOST': 'aws-1-ap-southeast-1.pooler.supabase.com',
+        'PORT': '5432',
     }
 }
 
