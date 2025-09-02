dbf0c026c2ee160066abc06121b3ff597416a4fa
Jirasak-Thongpire <tle0613828245@gmail.com>
2025-09-02 12:18:54 +0700

    config vercel

--- CashFlow_Tracker/wsgi.py
+++ CashFlow_Tracker/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CashFlow_Tracker.settings')
 
 application = get_wsgi_application()
+app = application  # For compatibility with some deployment setups
