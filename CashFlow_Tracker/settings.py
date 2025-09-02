0254f021fd83a1737dc06a03088693e35717d103
Jirasak-Thongpire <tle0613828245@gmail.com>
2025-09-02 12:00:13 +0700

    Add user registration functionality with custom user creation form

--- CashFlow_Tracker/settings.py
+++ CashFlow_Tracker/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/login/'
-LOGIN_REDIRECT_URL = '/dashboard/'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
 
 SESSION_COOKIE_AGE = 1209600
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from django.contrib import messages
 from datetime import datetime, timedelta
 from django.utils import timezone
+from .forms import CustomUserCreationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Registration successful! Welcome to CashFlow Tracker.')
+            return redirect('dashboard')
+    else:
+        form = CustomUserCreationForm()
+    
+    return render(request, 'accounts/register.html', {'form': form})
 
 @login_required
 def dashboard(request):
