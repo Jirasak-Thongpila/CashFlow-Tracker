25ce94df75324167a123bd4d22c9b1d4a8182eb1
Jirasak-Thongpire <tle0613828245@gmail.com>
2025-09-04 23:53:57 +0700

    set env

--- CashFlow_Tracker/settings.py
+++ CashFlow_Tracker/settings.py
@@ -10,9 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
+
 from django.core.exceptions import ImproperlyConfigured
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
 
 def get_env_variable(var_name, default=None):
     """Get an environment variable or raise an exception if not found."""
@@ -23,6 +29,7 @@ def get_env_variable(var_name, default=None):
             return default
         error_msg = f"Set the {var_name} environment variable"
         raise ImproperlyConfigured(error_msg)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,12 +96,12 @@ WSGI_APPLICATION = 'CashFlow_Tracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-#DATABASES = {
+# DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
-#}
+# }
 
 DATABASES = {
     'default': {
